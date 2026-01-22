f0ac1049620718c4180ebf1feb65b6a85ccbcde8
Your Name <your@email.com>
2026-01-22 16:34:35 +0100

    unfreeze det hyperhead

--- specs.py
+++ specs.py
@@ -204,11 +204,6 @@ def policy_spec_for_task(config, shard_size: int) -> ParamNodeSpec:
 def _srghn_filter_spec(srghn_module: eqx.Module):
     filter_spec = jax.tree_util.tree_map(eqx.is_array, srghn_module)
     filter_spec = eqx.tree_at(lambda m: m.stoch.basis, filter_spec, False)
-    filter_spec = eqx.tree_at(
-        lambda m: (m.det.linear.weight, m.det.linear.bias),
-        filter_spec,
-        (False, False),
-    )
     return filter_spec
 
 
